ebb7c7bc1130eb56a3c804b3b324619f11de3f55
Yavor Ivanov <davko996@gmail.com>
2014-07-29 05:20:43 +0300

    New Functions added

--- script.js
+++ script.js
@@ -15,36 +15,6 @@ ball = {
 		ctx.fill();
 	}
 };
-//Do this when collides == true
-function collideAction(ball, p) {
-	ball.vy = -ball.vy;
-	ballColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-	if(paddleHit == 1) {
-		ball.y = p.y - p.h;
-		particlePos.y = ball.y + ball.r;
-		multiplier = -1;	
-	}
-	
-	else if(paddleHit == 2) {
-		ball.y = p.h + ball.r;
-		particlePos.y = ball.y - ball.r;
-		multiplier = 1;	
-	}
-	
-	points++;
-	increaseSpd();
-	
-	if(collision) {
-		if(points > 0) 
-			collision.pause();
-			
-		collision.currentTime = 0;
-		collision.play();
-	}
-	
-	particlePos.x = ball.x;
-	flag = 1;
-}
 
 // Function to update positions, score and everything.
 // Basically, the main game logic is defined here
@@ -78,3 +48,66 @@ if (mouseOrKeyboard == "mouse"){
 		}
 	}
 }
+
+
+//Do this when collides == true
+function collideAction(ball, p) {
+	ball.vy = -ball.vy;
+	ballColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+	if(paddleHit == 1) {
+		ball.y = p.y - p.h;
+		particlePos.y = ball.y + ball.r;
+		multiplier = -1;	
+	}
+	
+	else if(paddleHit == 2) {
+		ball.y = p.h + ball.r;
+		particlePos.y = ball.y - ball.r;
+		multiplier = 1;	
+	}
+	
+	points++;
+	increaseSpd();
+	
+	if(collision) {
+		if(points > 0) 
+			collision.pause();
+			
+		collision.currentTime = 0;
+		collision.play();
+	}
+	
+	particlePos.x = ball.x;
+	flag = 1;
+}
+
+// Function for emitting particles
+function emitParticles() { 	
+	for(var j = 0; j < particles.length; j++) {
+		par = particles[j];		
+		ctx.beginPath(); 
+		ctx.fillStyle = ballColor;
+		if (par.radius > 0) {
+			ctx.arc(par.x, par.y, par.radius, 0, Math.PI*2, false);
+		}
+		ctx.fill();	 
+		
+		par.x += par.vx; 
+		par.y += par.vy; 
+		
+		// Reduce radius so that the particles die after a few seconds
+		par.radius = Math.max(par.radius - 0.05, 0.0); 		
+	} 
+}
+
+// Function for updating score
+function updateScore() {
+	ctx.fillStlye = "white";
+	ctx.font = "16px Arial, sans-serif";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Score: " + points, 20, 20 );
+}
+
+
+
