18afd05224b94bd9cb82a57851b51b058261e0db
Petar Genov <genov1824@gmail.com>
2014-07-30 15:13:30 +0300

    keyboard

--- script.js
+++ script.js
@@ -165,5 +165,100 @@ function updateScore() {
 	ctx.fillText("Score: " + points, 20, 20 );
 }
 
+var mouseOrKeyboard = "none";                                             
 
+// RequestAnimFrame: a browser API for getting smooth animations
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       || 
+		window.webkitRequestAnimationFrame || 
+		window.mozRequestAnimationFrame    || 
+		window.oRequestAnimationFrame      || 
+		window.msRequestAnimationFrame     ||  
+		function( callback ){
+			return window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+window.cancelRequestAnimFrame = ( function() {
+	return window.cancelAnimationFrame          ||
+		window.webkitCancelRequestAnimationFrame    ||
+		window.mozCancelRequestAnimationFrame       ||
+		window.oCancelRequestAnimationFrame     ||
+		window.msCancelRequestAnimationFrame        ||
+		clearTimeout
+} )();
+
+
+
+// Initialize canvas and required variables
+var canvas = document.getElementById("canvas"),
+		ctx = canvas.getContext("2d"), // Create canvas context --!>
+		W = window.innerWidth, // Window's width
+		H = window.innerHeight, // Window's height
+		particles = [], // Array containing particles
+		ball = {}, // Ball object
+		paddles = [2], // Array containing two paddles
+		mouse = {}, // Mouse object to store it's current position
+		points = 0, // Varialbe to store points
+		fps = 60, // Max FPS (frames per second)
+		particlesCount = 30, // Number of sparks when ball strikes the paddle
+		flag = 0, // Flag variable which is changed on collision
+		particlePos = {}, // Object to contain the position of collision 
+		multipler = 1, // Varialbe to control the direction of sparks
+		startBtn = {}, // Start button object
+		restartBtn = {}, // Restart button object
+		over = 0, // flag varialbe, cahnged when the game is over
+		init, // variable to initialize animation
+		paddleHit;
+		
+// Background image
+var ballColor = "white";
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "bg.jpg";
+
+// Add mousemove and mousedown events to the canvas
+canvas.addEventListener("mousemove", trackPosition, true);
+canvas.addEventListener("mousedown", btnClick, true);
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Initialise the collision sound
+collision = document.getElementById("collide");
+
+// Set the canvas's height and width to full screen
+canvas.width = W;
+canvas.height = H;
+
+// Function to paint canvas
+function paintCanvas() { 
+	 ctx.drawImage(bgImage, 0, 0, W,H);
+
+}
+
+// Function for creating paddles
+function Paddle(pos) {
+	// Height and width
+	this.h = 5;
+	this.w = 150;
+	
+	// Paddle's position
+	this.x = W/2 - this.w/2;
+	this.y = (pos == "top") ? 0 : H - this.h;
+	
+}
+
+// Push two new paddles into the paddles[] array
+paddles.push(new Paddle("bottom"));
+paddles.push(new Paddle("top")); 
 
